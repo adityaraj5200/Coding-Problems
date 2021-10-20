35a2f231f7024133efff8d553919678aa28482ba
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2021-10-21 02:52:33 +0530

    Update KMP algorithm.cpp

--- Important Algorithms/KMP algorithm.cpp
+++ Important Algorithms/KMP algorithm.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n=haystack.length(), m=needle.length(), cnt=0, i=1, j=0;
+        int n=haystack.length(), m=needle.length(), len=0, i=1, j=0;
         if(m==0) return 0;;
         vector<int> lps(m,0);
-        for(;i<m;i++){
-            if(needle[i] == needle[j]) cnt++,j++;
-            else j = cnt = 0;
-            
-            lps[i] = cnt;
+        while(i<m){
+            if(needle[i]==needle[len])
+                lps[i++] = ++len;
+            else{
+                if(len) len = lps[len-1];
+                else i++; 
+            }
         }
         
         i = j = 0;
-        while(i<n-m+1){
+        while(i<n){
             if(haystack[i]==needle[j]) i++,j++;
             else {
                 if(j!=0) j = lps[j-1];
